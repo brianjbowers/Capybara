f077e79755ac54b33731b3364f401a5f1a60eaa1
Brian Bowers <bbowers@berkeley.edu>
2026-01-16 12:37:56 -0800

    Update configuration settings in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,15 +1,4 @@
-# docs/conf.py
+version: 2
 
-project = "Capybara"
-author = "Capybara Project Authors"
-copyright = "2026, Capybara Project Authors"
-
-extensions = []
-
-templates_path = ["_templates"]
-exclude_patterns = []
-
-import sphinx_rtd_theme
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-extensions = ['sphinx_rtd_theme']
-html_theme = "sphinx_rtd_theme"
+# Default
+formats: []
