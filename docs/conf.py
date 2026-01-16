eb21907568ff33742da9ddc686d51f0a6f0b80a6
Brian Bowers <bbowers@berkeley.edu>
2026-01-16 12:15:00 -0800

    Change HTML theme to pydata_sphinx_theme

---
+++ docs/conf.py
@@ -0,0 +1,15 @@
+# docs/conf.py
+
+project = "Capybara"
+author = "Capybara Project Authors"
+copyright = "2026, Capybara Project Authors"
+
+extensions = []
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+#html_theme = "sphinx_rtd_theme"
+#html_theme = "alabaster"
+#html_theme = "furo"
+html_theme = "pydata_sphinx_theme"
