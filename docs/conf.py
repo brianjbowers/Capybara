165ca9c2fe916e8b9704b1dc289bb9fd726ce770
Brian Bowers <bbowers@berkeley.edu>
2026-01-16 12:20:26 -0800

    Add Sphinx RTD theme to documentation config

--- docs/conf.py
+++ docs/conf.py
@@ -9,4 +9,7 @@ extensions = []
 templates_path = ["_templates"]
 exclude_patterns = []
 
+import sphinx_rtd_theme
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+extensions = ['sphinx_rtd_theme']
 html_theme = "sphinx_rtd_theme"
